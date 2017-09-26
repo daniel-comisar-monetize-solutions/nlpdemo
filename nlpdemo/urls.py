5f99a2a8a183ef6eb0bafc3dfa3022b2063d405e
Daniel Comisar <daniel.comisar@monetizesolutions.com>
2017-09-26 10:40:26 -0700

    Use root url for demo app

--- nlpdemo/urls.py
+++ nlpdemo/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^demo/', include('nd.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('nd.urls')),
 ]
