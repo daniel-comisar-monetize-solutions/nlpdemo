0bd4fdf7c7340c3d199381337c867be9ac7e4c2a
Daniel Comisar <daniel.comisar@monetizesolutions.com>
2017-10-16 17:46:59 -0700

    Add script to extract everything possible

---
+++ extract_all.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <adobe.xml> <poppler.xml> <adobe.pdf>"
+    exit 1
+fi
+
+./extract_dtc.py $1
+./extract_fixes.py $1
+./extract_parts.py $1
+./extract_indexes.py $2
+./extract_numbers.py $2
+./extract_phrases.py $3
